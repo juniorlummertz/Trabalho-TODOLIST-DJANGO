16a2d7858d2d217501fee3661e8b95313576c073
Junior Lummertz <117364585+juniorlummertz@users.noreply.github.com>
2026-09-18 13:46:45 -0300

    fix: register task deletion route

--- app_tarefas/urls.py
+++ app_tarefas/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+
 from . import views
 
+
 urlpatterns = [
-    #URL vazia  direciona para a view lista_tarefas
-    path('',views.lista_tarefas, name='home'),
+    path('', views.lista_tarefas, name='home'),
     path('concluir/<int:tarefa_id>/', views.concluir_tarefa, name='concluir'),
-    ]
-path('deletar/<int:tarefa_id>/', views.deletar_tarefa, name='deletar'),
+    path('deletar/<int:tarefa_id>/', views.deletar_tarefa, name='deletar'),
+]
